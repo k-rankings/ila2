21c47b116365d37228a1a62c468c0c914eb852aa
boysplanet2022 <le.sserafim6m@gmail.com>
2024-06-13 15:13:11 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -465,7 +465,7 @@ var trainees = [];
 var filteredTrainees = [];
 // holds the ordered list of rankings that the user selects
 var ranking = newRanking();
-var rowNums = [1,2,4];
+var rowNums = [1,2,3];
 //window.addEventListener("load", function () {
   populateRanking();
   readFromCSV("./trainee_info.csv");
